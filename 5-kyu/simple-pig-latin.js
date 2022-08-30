502453afd72223e832994f5b1286e9d5d035ea04
Pavel Zdanovich <zdanovich.fti@gmail.com>
2022-08-30 11:31:37 +0300

    fix solvation

--- 5-kyu/simple-pig-latin.js
+++ 5-kyu/simple-pig-latin.js
@@ -1,8 +1,6 @@
 function pigIt(str) {
    let arr1 = str.split(' ');
-   let result = [];
    const eng = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
    for (let i = 0; i < arr1.length; i++) {
       arr1[i] = arr1[i].split('');
       if (arr1[i].length === 1 && eng.includes(arr1[i][0].toLowerCase(), 0)) {
@@ -13,7 +11,6 @@ function pigIt(str) {
          arr1[i].shift();
          arr1[i] = arr1[i].join('')
       }
-
    }
    return arr1.join(' ');
 }
